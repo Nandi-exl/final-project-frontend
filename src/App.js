cfe3fe61ef504742a6f1fa84b922b38f3f32c3d0
Nandi Naulfaldy <nandinaufaldi@gmail.com>
2022-06-11 15:48:30 +0700

    ssda

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [foods, setFood] = useState([]);
+  const [category, setCategory] = useState('');
+
+  const data = async () => {
+    const foods = await axios.get(`http://localhost:5000/food/1`);
+    setFood(foods.data[0]);
+    setCategory(foods.data[0].category.category);
+    setImages(foods.data[0].foodImages);
+  };
+  
+  useEffect(() => {
+    data();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{foods.foodName}</h1>
+      <p>{foods.description}</p>
+      <p>{category}</p>
+      {images.map((img) => {
+        const { id, image } = img;
+        return (
+          <>
+            <div key={id}>
+              <img
+                src={image}
+                alt={image}
+              />
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
