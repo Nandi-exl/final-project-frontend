415c1d86c95b383ef76374f8b2a2f7c3afe8ba4a
Nandi Naulfaldy <nandinaufaldi@gmail.com>
2022-06-16 12:51:49 +0700

    updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,69 +1,44 @@
-<<<<<<< HEAD
-import React from "react";
-import "./App.css";
-import Header from "./Components/Header/Header";
-import Home from "./Components/Home/Home";
-// import About from "./Components/About";
-import Food from "./Components/Food/Food";
-import Beverage from "./Components/Beverage/Beverage";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
 import './App.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
->>>>>>> main
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+// import About from "./Components/About";
+import Food from './Components/Food/Food';
+import Beverage from './Components/Beverage/Beverage';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import './App.css';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
 
 function App() {
-  const [images, setImages] = useState([]);
-  const [foods, setFood] = useState([]);
-  const [category, setCategory] = useState('');
+  //   const [images, setImages] = useState([]);
+  //   const [foods, setFood] = useState([]);
+  //   const [category, setCategory] = useState('');
+  // const data = async () => {
+  //   const foods = await axios.get(`http://localhost:5000/food/1`);
+  //   setFood(foods.data[0]);
+  //   setCategory(foods.data[0].category.category);
+  //   setImages(foods.data[0].foodImages);
+  // };
 
-  const data = async () => {
-    const foods = await axios.get(`http://localhost:5000/food/1`);
-    setFood(foods.data[0]);
-    setCategory(foods.data[0].category.category);
-    setImages(foods.data[0].foodImages);
-  };
-  
-  useEffect(() => {
-    data();
-  }, []);
+  // useEffect(() => {
+  //   data();
+  // }, []);
 
   return (
-<<<<<<< HEAD
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/food" element={<Food/>}/>
-          <Route path="/beverage" element={<Beverage/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/food' element={<Food />} />
+          <Route path='/beverage' element={<Beverage />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
-=======
-    <div className='App'>
-      <h1>{foods.foodName}</h1>
-      <p>{foods.description}</p>
-      <p>{category}</p>
-      {images.map((img) => {
-        const { id, image } = img;
-        return (
-          <>
-            <div key={id}>
-              <img
-                src={image}
-                alt={image}
-              />
-            </div>
-          </>
-        );
-      })}
->>>>>>> main
     </div>
   );
 }
